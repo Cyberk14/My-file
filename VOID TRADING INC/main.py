a79993ae52852bdeaffc5ddcbbc923a6cba2907d
Cyberk14 <139704497+Cyberk14@users.noreply.github.com>
2024-04-23 19:13:58 +0000

    updated on 23-Apr-24

--- VOID TRADING INC/main.py
+++ VOID TRADING INC/main.py
@@ -1,12 +1,2 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-options = Options()
-options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                          options=options)
-
-driver.get("https://www.python.org")
